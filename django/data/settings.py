3837c969a03022a1cffbb4c505434f8951cb9421
phoi5675 <phoi5675@gmail.com>
2022-01-30 23:32:14 +0900

    add db setting

--- django/data/settings.py
+++ django/data/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'data.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '192.168.0.30',
+        'PORT': '7000',
     }
 }
 
