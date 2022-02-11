4e9f8fc0c0606424a3b6e6993b3ab1b99053ae19
phoi5675 <phoi5675@gmail.com>
2022-02-12 00:07:21 +0900

    add image resize; update urls

--- django/webpage/views.py
+++ django/webpage/views.py
@@ -1,6 +1,7 @@
 import mimetypes
 from django.conf import settings
 from django.http import Http404, HttpResponse 
+from PIL import Image
 
 from .models import *
 
@@ -12,7 +13,7 @@ def boards(request, board_name: str) -> HttpResponse:
     query = Board.objects.filter(name=board_name).values()
     return HttpResponse(query, content_type='application/json')
 
-def article_markdown(request, title: str) -> HttpResponse:
+def article_markdown(request, board: str, title: str) -> HttpResponse:
     query = Article.objects.filter(title=title).first()
     article_path = query.file_name
     board_path = query.board_name.storage_path
@@ -24,19 +25,27 @@ def article_markdown(request, title: str) -> HttpResponse:
     except FileNotFoundError:
         raise Http404('File not found!')
 
-def article_image(request, title: str, img: str) -> HttpResponse:
+def article_image(request, board: str, title: str, img: str) -> HttpResponse:
     query = Article.objects.filter(title=title).first()
     article_path = query.file_name
     board_path = query.board_name.storage_path
 
     directory = settings.BOARD_DIR + board_path + article_path + '/' + 'img/'
     try:
-        with open(directory + img, 'rb') as f:
-            return HttpResponse(f.read(), content_type='image/jpeg')
+        MAX_IMG_SIZE = (500, 500)
+        img: Image = Image.open(directory + img)
+        img.thumbnail(MAX_IMG_SIZE)
+
+        response = HttpResponse(content_type='image/jpeg')
+        img.save(response, "JPEG")
+
+        return response
     except FileNotFoundError:
         raise Http404('File not found!')
+    except OSError as e:
+        raise Http404('Error during handling image')
 
-def article_attachment(request, title: str, attachment: str) -> HttpResponse:
+def article_attachment(request, board: str, title: str, attachment: str) -> HttpResponse:
     query = Article.objects.filter(title=title).first()
     article_path = query.file_name
     board_path = query.board_name.storage_path
