b010aa35481e6fcf5ec638500b44758a9f86b114
phoi5675 <phoi5675@gmail.com>
2022-04-17 00:42:06 +0900

    add test code for showing article list

--- django/webpage/boards/views.py
+++ django/webpage/boards/views.py
@@ -1,8 +1,11 @@
 import mimetypes
 from django.core import serializers
 from django.conf import settings
-from django.http import Http404, HttpResponse 
-from PIL import Image
+from django.http import Http404, HttpResponse
+
+import os
+from datetime import datetime
+
 
 from ..models import *
 
@@ -13,6 +16,16 @@ def boards(request) -> HttpResponse:
     return HttpResponse(data, content_type='application/json')
 
 def articles(request, board_name: str) -> HttpResponse:
+    # print('dir : ')
+    # dir = os.listdir(settings.BOARD_DIR + board_name)
+    # for sub_dir in dir:
+    #     path = f'{settings.BOARD_DIR}{board_name}/{sub_dir}'
+    #     stat = os.stat(path)
+    #     try:
+    #         print(f'created date : {datetime.utcfromtimestamp(int(stat.st_birthtime))}')
+    #     except AttributeError:
+    #         print(f'last modified : {datetime.utcfromtimestamp(int(stat.st_mtime))}')
+    
     query = Article.objects.filter(board_name=board_name)
     data = serializers.serialize('json', query)
 
